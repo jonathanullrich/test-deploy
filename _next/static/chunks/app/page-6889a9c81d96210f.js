39c4d7cbb5c98179ed26e1d5e48050a2e3a5ef2a
Jonathan Ullrich <jonathan.ullrich@iese.fraunhofer.de>
2023-07-04 09:16:47 +0200

    changes

---
+++ _next/static/chunks/app/page-6889a9c81d96210f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5995:function(n,e,t){Promise.resolve().then(t.t.bind(t,414,23)),Promise.resolve().then(t.t.bind(t,6903,23))},6903:function(){}},function(n){n.O(0,[414,667,139,744],function(){return n(n.s=5995)}),_N_E=n.O()}]);
